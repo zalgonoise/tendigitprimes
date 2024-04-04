5d79463418413b37eaadfcc5d23e42c0fbd35ed4
zalgonoise <mariozalgo@gmail.com>
2024-04-04 17:45:56 +0200

    tendigitprimes: scripts/build-sqlite: replaced database init logic

--- scripts/build-sqlite/main.go
+++ scripts/build-sqlite/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"flag"
-	"fmt"
 	"log/slog"
 	"os"
 
-	_ "modernc.org/sqlite"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/zalgonoise/tendigitprimes/database"
+	//_ "modernc.org/sqlite"
 )
 
-const uriFormat = "file:%s?cache=shared"
-
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource:   true,
+		Level:       nil,
+		ReplaceAttr: nil,
+	}))
 	ctx := context.Background()
 
 	code, err := run(logger)
@@ -33,35 +35,16 @@ func run(logger *slog.Logger) (int, error) {
 	flag.Parse()
 
 	logger.InfoContext(ctx, "validating output URI")
-	if err := validateURI(ctx, logger, *output); err != nil {
-		return 1, err
-	}
-
-	logger.InfoContext(ctx, "opening DB")
-	db, err := sql.Open("sqlite", fmt.Sprintf(uriFormat, *output))
+	db, err := database.OpenSQLite(*output, database.ReadWritePragmas(), logger)
 	if err != nil {
 		return 1, err
 	}
 
-	defer func() {
-		if err = db.Close(); err != nil {
-			logger.WarnContext(ctx, "failed to close database", slog.String("error", err.Error()))
-		}
-	}()
-
-	logger.InfoContext(ctx, "initializing database")
-	if err = initDatabase(ctx, logger, db); err != nil {
+	if err = database.MigrateSQLite(ctx, db, *input, logger); err != nil {
 		return 1, err
 	}
 
-	logger.InfoContext(ctx, "reading primes from input file(s)")
-	primes, err := readPrimes(ctx, logger, *input)
-	if err != nil {
-		return 1, err
-	}
-
-	logger.InfoContext(ctx, "inserting primes into DB")
-	if err = insertValues(ctx, logger, db, primes); err != nil {
+	if err = db.Close(); err != nil {
 		return 1, err
 	}
 
